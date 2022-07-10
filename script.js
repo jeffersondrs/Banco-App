81683ba54507754dd044af6a10f632382e30a1fc
jeffersondrs <jeffersonduarte@id.uff.br>
2022-07-10 20:18:28 -0300

    updating

--- script.js
+++ script.js
@@ -32,8 +32,33 @@ const account4 = {
   interestRate: 1,
   pin: 4444,
 };
-
-const accounts = [account1, account2, account3, account4];
+const account5 = {
+  owner: 'John Doe',
+  movements: [3300, -200, 1340, -3300, -120, 1150, 4200, -460],
+  interestRate: 1.5,
+  pin: 5555,
+};
+const account6 = {
+  owner: 'Jefferson Duarte Ramos Dos Santos',
+  movements: [22200, -2200, 12340, -5300, -120, 4050, 2400, -5460],
+  interestRate: 1.2,
+  pin: 6666,
+};
+const account7 = {
+  owner: 'Rafaela Moraes Cristino Duarte',
+  movements: [2500, -133, 7340, -1300, -620, 6650, 10400, -20460],
+  interestRate: 0.7,
+  pin: 7777,
+};
+const accounts = [
+  account1,
+  account2,
+  account3,
+  account4,
+  account5,
+  account6,
+  account7,
+];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -61,6 +86,36 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner.toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +129,58 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// filter
+
+const depositos = movements.filter(mov => mov > 0);
+const retiradas = movements.filter(mov => mov < 0);
+console.log(retiradas);
+console.log(depositos);
+
+// reduce
+
+const suma = movements.reduce((acc, mov) => acc + mov, 0);
+console.log(suma);
+
+
+// MAP - key value pairs
+
+// const conta = owner.forEach(function (owner) {
+//   if (inputLoginUsername === owner) {
+//     console.log(owner);
+//   } else {
+//     console.log('wrong');
+//   }
+// });
+
+// const loginName = function(username, pin) {
+//   const account = accounts.find(function (account) {
+//     return account.owner === username;
+//   }
+//   );
+//   if (account.pin === pin) {
+//     return account;
+//   }
+//   return null;
+// }
+
+// console.log(loginName);
+
+// const EuroToUSD = 1.2;
+// const EuroToGBP = 0.8;
+// const EuroToEUR = 1;
+// const USDToEUR = 1 / EuroToUSD;
+// const USDToGBP = 1 / EuroToGBP;
+// const USDToUSD = 1;
+// const GBPToUSD = 1 / USDToGBP;
+// const GBPToEUR = 1 / EuroToGBP;
+// const GBPToGBP = 1;
+
+// const movimentsUSD = movements.map(mov => mov * EuroToGBP);
+
+// console.log(movimentsUSD);
+
+// const movimentsTotal = movements.reduce(function (acc, mov) {
+//   return acc + mov;
+// });
+
+// console.log(movimentsTotal);
